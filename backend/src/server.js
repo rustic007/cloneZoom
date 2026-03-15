f536a81a1b938ff6e9d26ef03cef1a30feab6cd6
Ruslan <ruslanospanov0306@gmail.com>
2026-03-15 13:36:45 +0500

    Initialized repo

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import dotenv from "dotenv"
+import { ENV } from "./config/env.js";
+
+dotenv.config()
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(ENV.PORT, () => console.log(`The server is running on port: ${ENV.PORT}`)) 
