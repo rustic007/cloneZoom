95c30ec0b96a24755cf55090e5b7ec9ef70a3e5a
Ruslan <ruslanospanov0306@gmail.com>
2026-03-16 21:37:02 +0500

    the inngest.js fixed added es

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -10,11 +10,11 @@ const syncUser = inngest.createFunction(
     async ({event}) => {
         await connectDb()
 
-        const {id, email_address, first_name, last_name, image_url } = event.data
+        const {id, email_addresses, first_name, last_name, image_url } = event.data
 
         const newUser = {
             clerkId: id,
-            email: email_address[0]?.email_address,
+            email: email_addresses[0]?.email_address,
             name: `${first_name || ""} ${last_name || ""}`,
             image: image_url,
         }
